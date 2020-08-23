0e04ad5d1bfae74e0a6d579af0da22c52979a78a
acatzk <joshuaimalay@gmail.com>
2020-08-23 13:21:34 +0800

    Added dummy user-info style

--- src/components/pages/freedomwall/UserInfo.vue
+++ src/components/pages/freedomwall/UserInfo.vue
@@ -1,22 +1,35 @@
 <template>
     <div class="user-info">
-        <b-container>
-            <b-card 
-                title="User Information" 
-                id="card-about" 
-                class="d-flex"
-            >
-                <b-card-title>
-                    <b-icon icon="chat-quote"></b-icon> Joshua Galit
-                    <br>
-                    <span id="lbldate">
-                        Posted on <b-icon icon="alarm"></b-icon> 
-                    </span>
+        <b-card 
+            id="card-about" 
+            class="mb-3"
+        >
+            <b-row class="d-flex align-items-center">
+                <b-card-title class="d-flex align-items-center">
+                    <b-icon icon="person" font-scale="1"></b-icon>
+                    <b class="ml-2">Joshua Galit</b> 👋
                 </b-card-title>
-                <b-button>
-                    Logout
-                </b-button>
-            </b-card>
-        </b-container>
+                <b-dropdown class="ml-auto" text="@joshua" variant="outline-primary">
+                    <b-dropdown-item>Profile</b-dropdown-item>
+                    <b-dropdown-item>Logout</b-dropdown-item>
+                </b-dropdown>
+            </b-row>
+        </b-card>
     </div>
 </template>
+
+<script>
+
+    import { currentuser } from '@/services'
+
+    export default {
+        name: 'user-info',
+
+        data () {
+            return {
+                currentuser: currentuser
+            }
+        }
+        
+    }
+</script>
