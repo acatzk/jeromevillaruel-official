3bf31ef9934e386d8c277dc30a6b98310bf9a928
acatzk <joshuaimalay@gmail.com>
2020-08-23 12:20:34 +0800

    Create User Information authenticated

---
+++ src/components/pages/freedomwall/UserInfo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="user-info">
+        <b-container>
+            <b-card 
+                title="User Information" 
+                id="card-about" 
+                class="d-flex"
+            >
+                <b-card-title>
+                    <b-icon icon="chat-quote"></b-icon> Joshua Galit
+                    <br>
+                    <span id="lbldate">
+                        Posted on <b-icon icon="alarm"></b-icon> 
+                    </span>
+                </b-card-title>
+                <b-button>
+                    Logout
+                </b-button>
+            </b-card>
+        </b-container>
+    </div>
+</template>
