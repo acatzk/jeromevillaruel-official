611b278b6be8cd76218dac9be7e4d25fd1511a02
acatzk <joshuaimalay@gmail.com>
2020-08-24 15:57:03 +0800

    Set up open and close modal

---
+++ src/components/pages/auth/AuthDialog.vue
@@ -0,0 +1,27 @@
+<template>
+    <b-modal title="BootstrapVue" v-model="show">
+        <p class="my-4">Hello from modal!</p>
+    </b-modal>
+</template>
+
+<script>
+    export default {
+        name: 'auth-dialog',
+
+        props: ['visible'],
+
+        computed: {
+            show: {
+                get () {
+                    return this.visible
+                },
+                set (value) {
+                    if (!value) {
+                        this.$emit('close')
+                    }
+                }
+            }
+        }
+
+    }
+</script>
--- src/components/pages/auth/Authentication.vue
+++ src/components/pages/auth/Authentication.vue
@@ -1,7 +1,28 @@
 <template>
     <b-row>
-        <b-button block variant="primary" class="mx-3">
+        <b-button block variant="primary" class="mx-3" @click="dialog = !dialog">
             <b-icon icon="lock"></b-icon> Sign Up / Sign In
         </b-button>
+        
+        <auth-dialog 
+            :visible="dialog" @close="dialog = false"
+        />
+
     </b-row>
 </template>
+
+<script>
+    export default {
+        name: 'authentication',
+
+        components: {
+            AuthDialog: () => import('./AuthDialog')
+        },
+
+        data () {
+            return {
+                dialog: false
+            }
+        }
+    }
+</script>
