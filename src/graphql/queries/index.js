b0b33c22885c2899d305cb1d5200b2439dcc8b92
acatzk <joshuaimalay@gmail.com>
2020-08-25 16:21:00 +0800

    Added query for get single user in hasura

--- src/graphql/queries/index.js
+++ src/graphql/queries/index.js
@@ -1,94 +1,106 @@
 import gql from 'graphql-tag'
 
 export const GET_ALL_SUBSCRIBER_QUERY = gql`
-query getAllSubscriber {
-    villaruel_subscriber {
-      id
-      email
-    }
-  }
-`
-export const COUNT_ALL_SUBSCRIBER_QUERY = gql`
-query getCountSubscriber {
-    subscriberCount: villaruel_subscriber_aggregate {
-      aggregate {
-        count
+  query getAllSubscriber {
+      villaruel_subscriber {
+        id
+        email
       }
     }
+`
+export const COUNT_ALL_SUBSCRIBER_QUERY = gql`
+  query getCountSubscriber {
+      subscriberCount: villaruel_subscriber_aggregate {
+        aggregate {
+          count
+        }
+      }
   }  
 `
 
 export const GET_ALL_POSTS_FREEDOM_WALL = gql`
-query getAllPostFreedomWall {
-  freedom_wall(order_by: {created_at: desc}, limit: 20, offset: 0) {
-    id
-    name
-    posts
-    created_at
-    react: react_users_aggregate {
-      aggregate {
-        count
+  query getAllPostFreedomWall {
+    freedom_wall(order_by: {created_at: desc}, limit: 20, offset: 0) {
+      id
+      name
+      posts
+      created_at
+      react: react_users_aggregate {
+        aggregate {
+          count
+        }
+        nodes {
+          id
+          name
+        }
       }
-      nodes {
-        id
-        name
-      }
-    }
-    commentCount: comments_aggregate {
-      aggregate {
-        count
+      commentCount: comments_aggregate {
+        aggregate {
+          count
+        }
       }
     }
   }
-}
 `
 
 export const GET_SINGLE_USER_POST = gql`
-query getSingleUserPost ($post_id: uuid!){
-  freedom_wall(order_by: {created_at: desc}, where: {id: {_eq: $post_id}}) {
-    id
-    name
-    posts
-    created_at
-    react: react_users_aggregate {
-      aggregate {
-        count
+  query getSingleUserPost ($post_id: uuid!){
+    freedom_wall(order_by: {created_at: desc}, where: {id: {_eq: $post_id}}) {
+      id
+      name
+      posts
+      created_at
+      react: react_users_aggregate {
+        aggregate {
+          count
+        }
+        nodes {
+          id
+          name
+          created_at
+        }
       }
-      nodes {
-        id
-        name
-        created_at
-      }
-    }
-    commentCount: comments_aggregate {
-      aggregate {
-        count
-      }
-      nodes {
-        id
-        name
-        created_at
+      commentCount: comments_aggregate {
+        aggregate {
+          count
+        }
+        nodes {
+          id
+          name
+          created_at
+        }
       }
     }
   }
-}
 `
 
 export const GET_ALL_USER_COMMENT_BY_ID = gql`
-query getAllUserCommentByPostID ($post_id: uuid!) {
-  comments(where: {post_id: {_eq: $post_id}}) {
-    id
-    name
-    comment
-    created_at
+  query getAllUserCommentByPostID ($post_id: uuid!) {
+    comments(where: {post_id: {_eq: $post_id}}) {
+      id
+      name
+      comment
+      created_at
+    }
   }
-}
 `
 export const GET_ADMIN_PASSWORD = gql`
-query showPassword {
-  administrator {
-    id
-    password
+  query showPassword {
+    administrator {
+      id
+      password
+    }
   }
-}
+`
+
+export const GET_SINGLE_USER = gql`
+  query getSingleUser($firebase_id: String!) {
+    users(where: {firebase_id: {_eq: $firebase_id}}) {
+      id
+      firebase_id
+      name
+      username
+      email
+    }
+  }
 `
