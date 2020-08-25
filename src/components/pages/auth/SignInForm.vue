4e5ac98b028e234de1b905bdddfbe1be6bcbc4e4
acatzk <joshuaimalay@gmail.com>
2020-08-25 14:47:05 +0800

    Set up Form text and validation rules in vuelidate

--- src/components/pages/auth/SignInForm.vue
+++ src/components/pages/auth/SignInForm.vue
@@ -1,32 +1,96 @@
 <template>
-    <b-form>
+    <b-form @submit.prevent="onClickSignIn">
         <b-form-group
             label="Email"
         >
             <b-form-input
                 type="email"
-                required
                 id="txtpost" 
+                v-model.trim="$v.email.$model"
+                :class="{ 'is-invalid' : $v.email.$error, 'is-valid' : !$v.email.$invalid }"
             ></b-form-input>
+            <div class="invalid-feedback feedback">
+                <span v-if="!$v.email.required">Email is required</span>
+                <span v-if="!$v.email.email">Must be a valid email</span>
+            </div>
         </b-form-group>
 
         <b-form-group
             label="Password"
         >
             <b-form-input
-                required
                 id="txtpost" 
                 type="password"
+                v-model.trim="$v.password.$model"
+                :class="{ 'is-invalid' : $v.password.$error, 'is-valid' : !$v.password.$invalid }"
             ></b-form-input>
+            <div class="invalid-feedback feedback">
+                <span v-if="!$v.password.required">Password is required</span>
+            </div>
         </b-form-group>
-        
+
         <b-row>
             <b-col>
-                <b-button type="submit" block variant="primary">Login</b-button>
+                <b-button 
+                    type="submit" 
+                    block 
+                    variant="primary" 
+                    @click="onClickSignIn"
+                >Sign In</b-button>
             </b-col>
             <b-col>
-                <b-button type="reset" block variant="outline-secondary">Cancel</b-button>
+                <b-button 
+                    type="reset" 
+                    block 
+                    variant="outline-light" 
+                    @click="onClickReset"
+                >Cancel</b-button>
             </b-col>
         </b-row>
     </b-form>
 </template>
+
+<script>
+
+    import { required, email } from 'vuelidate/lib/validators'
+
+    export default {
+        name: 'sign-in-form',
+
+        data () {
+            return {
+                loading: false,
+                email: null,
+                password: null
+            }
+        },
+
+        
+        validations: {
+            email: {
+                required,
+                email
+            },
+            password: {
+                required
+            }
+        },
+
+        methods: {
+
+            onClickSignIn () {
+                this.$v.$touch()
+                if (!this.$v.$invalid) {
+                    alert('Goood')
+                    this.onClickReset()
+                }
+            },
+            onClickReset () {
+                this.$v.$reset()
+                this.email = null
+                this.password = null
+            }
+        }
+        
+    }
+</script>
--- src/components/pages/auth/SignUpForm.vue
+++ src/components/pages/auth/SignUpForm.vue
@@ -1,21 +1,33 @@
 <template>
-    <b-form>
+    <b-form @submit.prevent="onClickSignUp">
         <b-form-group
             label="Name"
         >
             <b-form-input
-                required
                 id="txtpost" 
+                v-model.trim="$v.name.$model"
+                :class="{ 'is-invalid' : $v.name.$error, 'is-valid' : !$v.name.$invalid }"
             ></b-form-input>
+            <div class="invalid-feedback feedback">
+                <span v-if="!$v.name.required">Name is required</span>
+                <span v-if="!$v.name.minLength">Name must have at least {{ $v.name.$params.minLength.min }} letters. </span>
+                <span v-if="!$v.name.maxLength">Name must have at most {{ $v.name.$params.maxLength.max }} letters.</span>
+            </div>
         </b-form-group>
 
         <b-form-group
             label="Username"
         >
             <b-form-input
-                required
                 id="txtpost" 
+                v-model.trim="$v.username.$model"
+                :class="{ 'is-invalid' : $v.username.$error, 'is-valid' : !$v.username.$invalid }"
             ></b-form-input>
+            <div class="invalid-feedback feedback">
+                <span v-if="!$v.username.required">Username is required</span>
+                <span v-if="!$v.username.minLength">Username must have at least {{ $v.username.$params.minLength.min }} letters. </span>
+                <span v-if="!$v.username.maxLength">Username must have at most {{ $v.username.$params.maxLength.max }} letters.</span>
+            </div>
         </b-form-group>
 
         <b-form-group
@@ -24,27 +36,108 @@
             <b-form-input
                 type="email"
                 id="txtpost" 
-                required
+                v-model.trim="$v.email.$model"
+                :class="{ 'is-invalid' : $v.email.$error, 'is-valid' : !$v.email.$invalid }"
             ></b-form-input>
+            <div class="invalid-feedback feedback">
+                <span v-if="!$v.email.required">Email is required</span>
+                <span v-if="!$v.email.email">Must be a valid email</span>
+            </div>
         </b-form-group>
 
         <b-form-group
             label="Password"
         >
             <b-form-input
-                required
                 id="txtpost" 
                 type="password"
+                v-model.trim="$v.password.$model"
+                :class="{ 'is-invalid' : $v.password.$error, 'is-valid' : !$v.password.$invalid }"
             ></b-form-input>
+            <div class="invalid-feedback feedback">
+                <span v-if="!$v.password.required">Password is required</span>
+                <span v-if="!$v.password.minLength">Password must have at least {{ $v.password.$params.minLength.min }} letters. </span>
+                <span v-if="!$v.password.maxLength">Password must have at most {{ $v.password.$params.maxLength.max }} letters.</span>
+            </div>
         </b-form-group>
 
         <b-row>
             <b-col>
-                <b-button type="submit" block variant="primary">Sign Up</b-button>
+                <b-button 
+                    type="submit" 
+                    block 
+                    variant="primary"
+                    @click="onClickSignUp"
+                >Sign Up</b-button>
             </b-col>
             <b-col>
-                <b-button type="reset" block variant="outline-secondary">Cancel</b-button>
+                <b-button 
+                    type="reset" 
+                    block 
+                    variant="outline-light"
+                    @click="onClickReset"
+                >Cancel</b-button>
             </b-col>
         </b-row>
     </b-form>
 </template>
+
+<script>
+
+    import { required, email, minLength, maxLength } from 'vuelidate/lib/validators'
+
+    export default {
+        name: 'sign-up-form',
+        
+        data () {
+            return {
+                loading: false,
+                name: null,
+                username: null,
+                email: null,
+                password: null
+            }
+        },
+
+        validations: {
+            name: {
+                required,
+                minLength: minLength(5),
+                maxLength: maxLength(25)
+            },
+            username: {
+                required,
+                minLength: minLength(5),
+                maxLength: maxLength(20)
+            },
+            email: {
+                required,
+                email
+            },
+            password: {
+                required,
+                minLength: minLength(5),
+                maxLength: maxLength(20)
+            }
+        },
+
+        methods: {
+            onClickSignUp () {
+                this.$v.$touch()
+                if (!this.$v.$invalid) {
+                    this.onClickReset()
+                }
+            },
+
+            onClickReset () {
+                this.name = null
+                this.username = null
+                this.email = null
+                this.password = null
+                this.$v.$reset()
+            }
+        }
+
+
+    }
+</script>
